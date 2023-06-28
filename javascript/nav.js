c7164fe3c511a6a398eb4a5a5fa25d1496c3b282
inyangj <inyangjohnson01@gmail.com>
2023-06-28 10:03:27 +0100

    Added the dashboard page and ui

---
+++ javascript/nav.js
@@ -0,0 +1,10 @@
+document.getElementById("hamburger-btn").addEventListener("click", function() {
+    let menu = document.getElementById("nav");
+    menu.classList.toggle("show");
+});
+
+
+document.getElementById("hamBtn").addEventListener("click", function() {
+    let menu = document.getElementById("nav");
+    menu.classList.toggle("show");
+});
