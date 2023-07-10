fb6efc016b01653e44b83ffd1b285f0fc6510a30
victoryemmanuel <emmanuelvictorry@gmail.com>
2023-07-10 15:02:43 +0100

    updated my logout.js

--- javascript/logout.js
+++ javascript/logout.js
@@ -1,11 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const logoutButton = document.querySelector('.logout-button');
-  
-    logoutButton.addEventListener('click', handleLogout);
-  
-    function handleLogout() {
-      sessionStorage.removeItem('user');
-      window.location.href = '../index.html'; // Redirect to the login page after logout
-    }
-  });
-  
+document.getElementById('logoutdiv').addEventListener('click', function() {
+    sessionStorage.clear();
+    window.location.href = '../index.html'; 
+});
